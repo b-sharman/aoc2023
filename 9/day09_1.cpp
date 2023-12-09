e82dcce870b13a7344672330157025af07f2b150
b-sharman <nonoesimposible72@gmail.com>
2023-12-08 22:58:01 -0700

    does GitHub recognize markdown in commit messages?
    This is the first day where I felt like I was held back by the language much more than my brain; I could've been much faster in Python. I lost a lot of time in these areas:
    
    * parsing - took 20 minutes to write the parser, whereas in Python it would have just been `map(int, input().split())`
    * iterating - I iterated backwards over an array with `size_t i; ...; i--` and it took me a while to spot the overflow error
    * syntax - I did `for (...) a; b;` without braces and didn't realize for a while why `b` wasn't being called when I thought it was

---
+++ 9/day09_1.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    size_t nl, nn;
+    cin>>nl>>nn;
+
+    string s;
+    ll temp;
+    vector<vector<ll>> histories;
+    vector<ll> history;
+    for (size_t i=0; i<nl; i++) {
+        history.clear();
+        for (size_t j=0; j<nn; j++) { cin>>temp; history.push_back(temp); }
+        histories.push_back(history);
+    }
+
+    vector<ll> answers;
+    for (const auto& history : histories) {
+        vector<vector<ll>> diffs;
+        diffs.push_back(history);
+        vector<ll> diff {-1};
+        while (!diff.empty() && !all_of(diff.begin(), diff.end(), [](const auto& v){return v==0;})) {
+            diff.clear();
+            for (size_t i=1; i<diffs.back().size(); i++) {
+                diff.push_back(diffs.back().at(i) - diffs.back().at(i-1));
+                cout << diff.back() << " ";
+            }
+            cout << endl;
+            diffs.push_back(diff);
+        }
+
+        vector<ll> ends {0};
+        for (auto it=prev(diffs.end(), 2); it>=diffs.begin(); advance(it, -1)) {
+            ends.push_back((*it).back() + ends.back());
+        }
+        cout << "end: " << ends.back() << endl;
+        answers.push_back(ends.back());
+    }
+    cout << accumulate(answers.begin(), answers.end(), 0) << endl;
+
+    return 0;
+}
