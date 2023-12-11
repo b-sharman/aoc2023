709d159ae78d65b8221a0e98ab476543aa5d12aa
b-sharman <nonoesimposible72@gmail.com>
2023-12-11 00:01:25 -0700

    numpy for day 11
    I lost like 20 minutes going in the wrong direction, and my excuse for
    the rest of the time is that my limited numpy knowledge required
    extensive Googling. An 8-sloc solution for day 11 (pre-golfing) was
    fun to write, especially after my C++ knowledge has required 100 to
    200 sloc for previous days.

---
+++ 11/day11_1.py
@@ -0,0 +1,10 @@
+import sys
+import numpy as np
+
+with open(sys.argv[-1]) as f:
+    image = np.array([list(row) for row in f.read().strip().split("\n")])
+
+empty_rows = [(row == ".").all() for row in image]
+empty_cols = [(col == ".").all() for col in image.transpose()]
+galaxies = [g + np.array((sum(empty_rows[:g[0]]), sum(empty_cols[:g[1]]))) for g in np.transpose(np.where(image == "#"))]
+print(sum(np.abs(g0 - g1).sum() for i, g0 in enumerate(galaxies) for g1 in galaxies[i+1:]))
